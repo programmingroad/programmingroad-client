54789d796377141dd3b2e60e0448d64f4e54d627
programmingroad <programmingroad@163.com>
2019-11-12 22:32:09 +0800

    .

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom'
 import Login from "./pages/login";
 import Admin from "./pages/admin";
 import Blog from "./pages/blog";
+import {ConfigProvider} from "antd";
+import zhCN from 'antd/es/locale/zh_CN';
 
 class App extends Component {
 
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/login' component={Login}/>
-                    <Route path='/admin' component={Admin}/>
-                    <Route exact path='/' component={Blog}/>
-                    <Redirect to='/'/>
-                </Switch>
-            </BrowserRouter>
+            <ConfigProvider locale={zhCN}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/login' component={Login}/>
+                        <Route path='/admin' component={Admin}/>
+                        <Route exact path='/' component={Blog}/>
+                        <Redirect to='/'/>
+                    </Switch>
+                </BrowserRouter>
+            </ConfigProvider>
         );
     }
 }
--- src/pages/tags/index.jsx
+++ src/pages/tags/index.jsx
@@ -1,18 +1,62 @@
 import React, {Component} from "react";
-import {Card, Tag} from "antd";
+import {Card, Modal, Table} from "antd";
+
+
+const {Column} = Table;
+const {confirm} = Modal;
 
 export default class Tags extends Component {
 
-    onClose(e){
-        console.log(e)
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {
+                    "id": 4,
+                    "name": "java"
+                },
+                {
+                    "id": 5,
+                    "name": "spring boot"
+                },
+                {
+                    "id": 6,
+                    "name": "react"
+                }
+            ]
+        }
+    }
+
+    deleteTag = (id) => {
+        confirm({
+            title: '删除标签?',
+            onOk: () => {
+                this.setState({
+                    data: this.state.data.filter(item => item.id !== id)
+                })
+            },
+            onCancel() {
+            },
+        })
     }
 
     render() {
+
+        const {data} = this.state;
+
         return (
-            <Card title="标签管理" bordered={false} style={{height: "100%"}}>
-                <Tag closable onClose={this.onClose}>123</Tag>
-                <Tag closable onClose={this.onClose}>123122222222222222222222222222</Tag>
-                <Tag closable onClose={this.onClose}>123</Tag>
+            <Card title={"标签管理"} style={{minHeight: "100%"}}>
+                <Table dataSource={data} pagination={false}>
+                    <Column title="名称" dataIndex="name" key="name"/>
+                    <Column
+                        title="操作"
+                        key="action"
+                        width={"300px"}
+                        render={(text, record) => (
+                            <a onClick={() => this.deleteTag(record.id)}>删除</a>
+                        )}
+                    />
+                </Table>
             </Card>
         )
     }
