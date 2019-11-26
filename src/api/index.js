24bf402f32d97bd4c733a6eda8052ac08c3ba624
baoqi.liu <baoqi.liu@yitu-inc.com>
2019-11-26 12:13:20 +0800

    标签管理

--- src/api/index.js
+++ src/api/index.js
@@ -3,27 +3,60 @@ import ajax from './ajax'
 // const BASE = 'http://localhost:80'
 const BASE = ''
 
-// user
+/**
+ * user
+ *
+ * @param code
+ * @returns {AxiosPromise}
+ */
+
+// 登录
 export const reqLogin = (code) => ajax({
     url: BASE + '/user/login',
     method: 'POST',
     data: code
 })
 
+// 注销
 export const reqLogout = () => ajax({
     url: BASE + '/user/logout',
     method: 'GET'
 })
 
+// 获取用户信息
 export const reqGetUser = () => ajax({
     url: BASE + '/user/user',
     method: 'GET'
 })
 
-// tag
+/**
+ * tag
+ *
+ * @returns {AxiosPromise}
+ */
+
+// 获取所有标签
 export const reqAllTag = () => ajax({
     url: BASE + '/tag/all',
     method: 'GET'
+})
+
+// 删除标签
+export const reqDeleteTag = (id) => ajax({
+    url: BASE + '/tag/delete',
+    method: 'POST',
+    params: {
+        id
+    }
+})
+
+// 添加标签
+export const reqAddTag = (name) => ajax({
+    url: BASE + '/tag/add',
+    method: 'POST',
+    params: {
+        name
+    }
 })
 
 
--- src/pages/tags/index.jsx
+++ src/pages/tags/index.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
-import {Card, Modal, Table} from "antd";
+import {Button, Card, Input, Modal, Table} from "antd";
+import {reqAddTag, reqAllTag, reqDeleteTag} from "../../api";
+
+import './index.less'
 
 
 const {Column} = Table;
@@ -10,48 +13,93 @@ export default class Tags extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [
-                {
-                    "id": 4,
-                    "name": "java"
-                },
-                {
-                    "id": 5,
-                    "name": "spring boot"
-                },
-                {
-                    "id": 6,
-                    "name": "react"
-                }
-            ]
+            inputValue: '',
+            data: [],
+            visible: false
         }
     }
 
+    // 获取标签
+    getTags = async () => {
+        const data = await reqAllTag();
+        this.setState({
+            data: data.body
+        })
+    }
+
+    // 删除标签
     deleteTag = (id) => {
         confirm({
-            title: '删除标签?',
-            onOk: () => {
-                this.setState({
-                    data: this.state.data.filter(item => item.id !== id)
-                })
+            title: '删除标签',
+            onOk: async () => {
+                await reqDeleteTag(id);
+                this.getTags();
             },
             onCancel() {
             },
         })
     }
 
+    // 点击确认
+    onOk = async () => {
+        const {inputValue} = this.state;
+        await reqAddTag(inputValue);
+        this.getTags();
+        this.setState({
+            visible: false,
+            inputValue: ''
+        });
+    };
+
+    // 显示添加标签的modal
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    // 隐藏添加标签的modal
+    hideModal = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
+    // 改变inputValue
+    onChange = (e) => {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    // 页面render后获取全部的标签
+    componentDidMount() {
+        this.getTags();
+    }
+
     render() {
-
-        const {data} = this.state;
-
+        const {data, inputValue} = this.state;
         return (
             <Card title={"标签管理"} style={{minHeight: "100%"}}>
+                <div className={'tag-header'}>
+                    <Button type="primary" style={{marginLeft: '30px'}} onClick={this.showModal}>添加标签</Button>
+                    <Modal
+                        title="添加标签"
+                        visible={this.state.visible}
+                        onOk={this.onOk}
+                        onCancel={this.hideModal}
+                        okText="确认"
+                        cancelText="取消"
+                    >
+                        <Input placeholder="标签名称" onChange={this.onChange} value={inputValue}/>
+                    </Modal>
+                </div>
                 <Table dataSource={data} pagination={false}>
                     <Column title="名称" dataIndex="name" key="name"/>
+                    <Column title="创建时间" dataIndex="createTime" key="createTime"/>
                     <Column
                         title="操作"
                         key="action"
-                        width={"300px"}
                         render={(text, record) => (
                             <a onClick={() => this.deleteTag(record.id)}>删除</a>
                         )}
@@ -60,5 +108,4 @@ export default class Tags extends Component {
             </Card>
         )
     }
-
 }
