629ba9477589680c2c60013fca6dedfbe96873a4
baoqi.liu <baoqi.liu@yitu-inc.com>
2019-11-29 19:10:09 +0800

    .

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,6 @@ import ajax from './ajax'
 // const BASE = 'http://localhost:80'
 const BASE = ''
 
-/**
- * user
- *
- * @param code
- * @returns {AxiosPromise}
- */
-
 // 登录
 export const reqLogin = (code) => ajax({
     url: BASE + '/user/login',
@@ -28,12 +21,6 @@ export const reqGetUser = () => ajax({
     url: BASE + '/user/user',
     method: 'GET'
 })
-
-/**
- * tag
- *
- * @returns {AxiosPromise}
- */
 
 // 获取所有标签
 export const reqAllTag = () => ajax({
@@ -55,5 +42,21 @@ export const reqAddTag = (name) => ajax({
         name
     }
 })
+
+// 添加文章
+export const reqAddArticle = (article) => ajax({
+    url: BASE + '/article/add',
+    method: 'PUT',
+    data: article
+})
+
+// 删除文章
+export const reqDeleteArticle = () => ajax({})
+
+// 获取文章列表
+export const reqArticleList = () => ajax({})
+
+// 更新文章
+export const reqUpdateArticle = () => ajax({})
 
 
--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -1,82 +1,111 @@
 import React, {Component} from "react";
-import {Button, Card, Dropdown, Icon, Input, Menu, Modal} from "antd";
+import {Button, Card, Input, Menu, Modal, Select} from "antd";
 import ReactMarkdown from "react-markdown";
 
 import './index.less'
+import {reqAllTag} from "../../api";
 
-const menu = (
-    <Menu>
-        <Menu.Item key="1">
-            1st menu item
-        </Menu.Item>
-        <Menu.Item key="2">
-            2nd menu item
-        </Menu.Item>
-        <Menu.Item key="3">
-            3rd item
-        </Menu.Item>
-    </Menu>
-);
+
+const {Option} = Select;
 
 export default class Edit extends Component {
 
     constructor(props) {
         super(props)
         this.state = ({
-            value: "",
-            visible: false
+            title: "",
+            content: "",
+            visible: false,
+            selectTagId: 0,
+            tagList: []
         })
     }
 
-    showModal = () => {
+    getTagList = async () => {
+        const data = await reqAllTag();
+        this.setState({
+            tagList: data.body
+        })
+    }
+
+    componentDidMount() {
+        this.getTagList();
+    }
+
+    preview = () => {
         this.setState({
             visible: true,
         });
     };
 
-    hideModal = () => {
+    cancelPreview = () => {
         this.setState({
             visible: false,
         });
     };
 
-    onChange = (e) => {
-        console.log(e)
+    changeContent = (e) => {
         this.setState({
-            value: e.target.value
+            content: e.target.value
         })
+    }
+
+    changeTitle = (e) => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    changeSelect = (value) => {
+        this.setState({
+            selectTagId: value
+        })
+    }
+
+    save = () => {
+        console.log(this.state)
+    }
+
+    release = () => {
+        console.log(this.state)
     }
 
     render() {
 
-        const {value} = this.state;
+        const {content, title, tagList} = this.state;
+
         return (
             <Card title={"写博客"} style={{height: "100%"}}>
                 <div className={"edit-header"}>
-                    <Input placeholder={"请输入标题"} style={{marginRight: '30px'}}/>
-                    <Dropdown overlay={menu}>
-                        <Button>
-                            请选择标签 <Icon type="down"/>
-                        </Button>
-                    </Dropdown>
-                    <Button type="primary" style={{marginLeft: '30px'}} onClick={this.showModal}>预览</Button>
+                    <Input placeholder={"请输入标题"} style={{marginRight: '30px'}} value={title}
+                           onChange={this.changeTitle}/>
+                    <Select defaultValue="请选择标签" style={{width: 180}} onChange={this.changeSelect}>
+                        {
+                            tagList.map(
+                                item => {
+                                    return <Option value={item.id}>{item.name}</Option>
+                                }
+                            )
+                        }
+                    </Select>
+                    <Button type="primary" style={{marginLeft: '30px'}} onClick={this.preview}>预览</Button>
                     <Modal
                         title="预览"
                         visible={this.state.visible}
-                        onOk={this.hideModal}
-                        onCancel={this.hideModal}
+                        onOk={this.cancelPreview}
+                        onCancel={this.cancelPreview}
                         footer={null}
                         wrapClassName={'edit-modal'}
                     >
-                        <ReactMarkdown className={''} source={value}/>
+                        <ReactMarkdown source={content}/>
                     </Modal>
-                    <Button type="primary" style={{marginLeft: '10px'}}>保存</Button>
-                    <Button type="primary" style={{marginLeft: '10px'}}>发布</Button>
+                    <Button type="primary" style={{marginLeft: '10px'}} onClick={this.save}>保存</Button>
+                    <Button type="primary" style={{marginLeft: '10px'}} onClick={this.release}>发布</Button>
                 </div>
                 <textarea
                     className={"edit-content"}
-                    onChange={this.onChange}
-                    value={value}
+                    onChange={this.changeContent}
+                    value={content}
                 >
                 </textarea>
             </Card>
